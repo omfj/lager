65be8d0b5a7bee7db09deefe4f121f932c6f0d8b
Ole Magnus Fon Johnsen <me@omfj.no>
2024-11-04 16:14:13 +0100

    Add compouted equality fn test

--- packages/stand/src/computed.test.ts
+++ packages/stand/src/computed.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { computed } from "./computed.js";
 import { signal } from "./signal.js";
 
@@ -12,5 +12,33 @@ describe("Computed", () => {
     s.value = 2;
 
     expect(c.value).toBe(4);
+  });
+
+  it("should not trigger when value is the same", () => {
+    const s = signal(1);
+    const c = computed(() => s.value * 2);
+
+    expect(c.value).toBe(2);
+
+    s.value = 1;
+
+    expect(c.value).toBe(2);
+  });
+
+  it("should use custom equality function", () => {
+    const s = signal([1, 2]);
+    const c1 = computed(
+      () => s.value,
+      (a, b) => a.length === b.length
+    );
+    const c2 = computed(() => c1.value);
+    const spy = vi.spyOn(c1, "notify");
+
+    expect(c2.value).toStrictEqual([1, 2]);
+
+    s.value = [2, 1];
+
+    expect(c2.value).toStrictEqual([1, 2]);
+    expect(spy).toHaveBeenCalledOnce();
   });
 });
