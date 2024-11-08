fb06dfd13a16f040aa7bfca04466c2ece7cd78ec
Ole Magnus Fon Johnsen <me@omfj.no>
2024-11-08 14:23:41 +0100

    Add more reactive libraries to benchmarks

--- packages/benchmarks/src/computed.bench.ts
+++ packages/benchmarks/src/computed.bench.ts
@@ -1,6 +1,8 @@
 import { bench } from "vitest";
 import { signal, computed } from "stand";
 import { Signal } from "signal-polyfill";
+import { reactive } from "@reactively/core";
+import { ref } from "@vue/reactivity";
 
 bench("computed", () => {
   const s = signal(1);
@@ -12,4 +14,16 @@ bench("computed - polyfill", () => {
   const s = new Signal.State(1);
   const c = new Signal.Computed(() => s.get() * 2);
   s.set(2);
+});
+
+bench("computed - reactively", () => {
+  const s = reactive(1);
+  const c = computed(() => s.value * 2);
+  s.value = 2;
+});
+
+bench("computed - vue", () => {
+  const s = ref(1);
+  const c = computed(() => s.value * 2);
+  s.value = 2;
 });
--- packages/benchmarks/src/signal.bench.ts
+++ packages/benchmarks/src/signal.bench.ts
@@ -1,6 +1,8 @@
 import { bench } from "vitest";
 import { signal } from "stand";
 import { Signal } from "signal-polyfill";
+import { reactive } from "@reactively/core";
+import { ref } from "@vue/reactivity";
 
 bench("signal", () => {
   const state = signal(0);
@@ -10,4 +12,14 @@ bench("signal", () => {
 bench("signal - polyfill", () => {
   const state = new Signal.State(0);
   state.set(1);
+});
+
+bench("signal - reactively", () => {
+  const state = reactive(0);
+  state.value = 1;
+});
+
+bench("signal - vue", () => {
+  const state = ref(0);
+  state.value = 1;
 });
